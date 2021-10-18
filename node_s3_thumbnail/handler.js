f65874840aef34bcebcf299502dc42169ce599e7
Rdunei Oliveira <rhoodsk8erboy@gmail.com>
2021-10-18 15:10:17 -0300

    service to transform image on thumnail is created

--- node_s3_thumbnail/handler.js
+++ node_s3_thumbnail/handler.js
@@ -1,18 +1,54 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+const AWS = require("aws-sdk");
+const util = require("util");
+const sharp = require("sharp");
+
+const s3 = new AWS.S3();
+
+const sourceBucket = process.env.SOURCE_BUCKET;
+const destinationBucket = process.env.DESTINATION_BUCKET;
+const sizes = {
+  width: Number(process.env.SIZES),
+  height: Number(process.env.SIZES),
+};
+
+module.exports.thumbGenerator = async (event) => {
+  const record = event.Records[0].s3;
+  const key = record.object.key;
+
+  const img = await getS3Image(key);
+  const imgResized = await resizeImage(img);
+  const url = await uploadToS3(imgResized, key);
+
+  return url;
+};
+
+function getS3Image(keybkt) {
+  const result = s3.getObject({ Bucket: sourceBucket, Key: keybkt }).promise();
+
+  return result;
+}
+
+function resizeImage(data) {
+  const result = sharp(data.Body)
+    .resize(sizes.width, sizes.height)
+    .toFormat("png")
+    .toBuffer();
+
+  return result;
+}
+
+function uploadToS3(buffer, keybkt) {
+  const params = {
+    Key: String(keybkt),
+    ACL: "public-read",
+    Bucket: destinationBucket,
+    Body: buffer,
+    ContentType: "image/png",
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  const result = s3.putObject(params).promise();
+
+  return result;
+}
